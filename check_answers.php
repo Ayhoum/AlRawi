1b33f2210bd0f467f1ae0626eb67076a13ac2bab
Alaa Semsemea <semsemea.a@gmail.com>
2017-12-12 00:41:07 +0100

    Alaa

--- check_answers.php
+++ check_answers.php
@@ -55,6 +55,8 @@ include 'scripts/db_connection.php';
 
     <link rel="stylesheet" href="css/colors.php?color=DE6262" type="text/css" />
 
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
 
     <style>
         .form-control.error { border: 2px solid red; }
@@ -135,101 +137,225 @@ include 'scripts/db_connection.php';
             <h4>الاسئلــة</h4>
 
     <?php
-    if (isset($_GET)){
+    if (isset($_GET['id'])){
 
-    $qId = 1;
+    $qId = $_GET['id'];
 
-    // GET THE QUESTION FROM DB
+    $beginValue  = (($qId -1) * 65) + 1;
+
+
+
+
+    if (isset($_POST['submit'])){
+
+        for ($i = 1; $i <= 65; $i++){
+
+            if(!empty($_POST['selector'.$i]))
+            {
+
+                ${'selector_' . $i} = $_POST['selector'.$i];
+
+            } else {
+
+                ${'selector_' . $i} = "0";
+
+            }
+
+
+
+        }
+
+
+
+
+        // GET THE QUESTION FROM DB
     $query = " SELECT * FROM `FREE_EXAM_QUESTION` WHERE `FREE_QUESTION_SET_ID` = '{$qId}'";
     $result = mysqli_query($mysqli, $query);
     if (mysqli_num_rows($result) > 0) {
+        $x = 1;
 
 
     while ($row = mysqli_fetch_assoc($result)) {
 
-    $number = $row['NUMBER'];
-    $question = $row['QUESTION'];
-    $right_answer = $row['RIGHT_ANSWER'];
-    $second_answer = $row['ANSWER_2'];
-    $third_answer = $row['ANSWER_3'];
-    $forth_answer = $row['ANSWER_4'];
-    $picture = $row['REASON'];
-    $reason = $row['PICTURE'];
-    $type = $row['TYPE'];
+    $number          = $row['NUMBER'];
+    $number = $number - ($qId - 1) * 65;
+    $question        = $row['QUESTION'];
+    $right_answer    = $row['RIGHT_ANSWER'];
+    $second_answer   = $row['ANSWER_2'];
+    $third_answer    = $row['ANSWER_3'];
+    $forth_answer    = $row['ANSWER_4'];
+    $picture         = $row['PICTURE'];
+    $reason          = $row['REASON'];
+    $type            = $row['TYPE'];
 
 
-    ?>
+    if ( ${'selector_' . $x} == $right_answer ){
+        ?>
+        <div class="tabs side-tabs tabs-bordered clearfix" id="tab-5">
 
+            <ul class="tab-nav clearfix">
+                <li><a href="#">  <?php echo "السؤال رقم: " .$number ;?> <i class="fa fa-check" aria-hidden="true"></i> </a></li>
+            </ul>
 
-            <div class="tabs side-tabs tabs-bordered clearfix" id="tab-5">
+            <div class="tab-container">
 
-                <ul class="tab-nav clearfix">
-                    <li><a href="#tabs-17">  <?php echo "السؤال رقم: " .$number ;?></a></li>
-                </ul>
+                <div class="tab-content clearfix" style="direction: rtl" id="tabs-17">
 
-                <div class="tab-container">
+                    <div class="fancy-title title-bottom-border">
+                        <h3>السؤال:  <span><?php    echo $question ; ?> </span> </h3>
+                    </div>
 
-                    <div class="tab-content clearfix" style="direction: rtl" id="tabs-17">
+                    <div class="row" >
+                        <div class="col-sm-7">
+                            <img src="<?php echo $picture ?>" class="img-fluid img-thumbnail" >
 
-                        <div class="fancy-title title-bottom-border">
-                            <h3>السؤال:  <span><?php    echo $question ; ?> </span> </h3>
                         </div>
 
-                        <div class="row" >
-                            <div class="col-sm-7">
-                                <img src="<?php echo $picture ?>" class="img-fluid img-thumbnail" >
-
-                            </div>
-
-                            <div class="col-sm-5">
+                        <div class="col-sm-5">
                             <h4> الاجابات : </h4>
                             <?php
-                                if ($type == "response"){
-    
-                                    echo " الاجابة الأولى : فرامل" .  "<br>";
-                                    echo " الاجابة االثانية : رفع قدم عن الوقود" . "<br>";
-                                    echo " الاجابة الثالثة : لا شئ" .  "<br>";
-    
-                                } elseif ($type == "yesNo"){
-    
-                                    echo " الاجابة الأولى : نعم " . "<br>";
-                                    echo " الاجابة االثانية : لا " . "<br>";
-    
-                                } elseif ($type == "numInp"){
-                                    echo " الاجابة الصحيحة : " . $right_answer. "<br>";
-    
-                                }elseif ($type == "multiChoice"){
-    
-    
-                                    echo " الاجابة الأولى : " . $right_answer. "<br>";
-                                    echo " الاجابة االثانية : " . $second_answer. "<br>";
-    
-                                    if ($third_answer != "0") {
-    
-                                        echo " الاجابة الثالثة : " . $third_answer . "<br>";
-                                    }
-    
-                                    if ($forth_answer != "0"){
-    
-                                    echo " الاجابة الرابعة : " . $forth_answer. "<br>";
-    
-                                    }
-    
-                                }
-    
-    
-    
-                            ?>
-                            </div>
-                        </div>
+                            if ($type == "response"){
+                                ?>
 
+
+                                <p <?php if ($right_answer == "فرامل"){ echo "style= 'color: green'";} ?> >فرامل</p>
+                                <p <?php if ($right_answer == "رفع قدم عن الوقود"){ echo "style= 'color: green'";} ?>>رفع قدم عن الوقود</p>
+                                <p <?php if ($right_answer == "لا شئ"){ echo "style= 'color: green'";} ?>>لا شيء</p>
+
+
+                                        <?php
+                            } elseif ($type == "yesNo"){
+
+                                echo " الاجابة الأولى : نعم " . "<br>";
+                                echo " الاجابة االثانية : لا " . "<br>";
+
+                            } elseif ($type == "numInp"){
+                                echo " الاجابة الصحيحة : " . $right_answer. "<br>";
+
+                            }elseif ($type == "multiChoice"){
+
+
+                                echo " الاجابة الأولى : " . $right_answer. "<br>";
+                                echo " الاجابة االثانية : " . $second_answer. "<br>";
+
+                                if ($third_answer != "0") {
+
+                                    echo " الاجابة الثالثة : " . $third_answer . "<br>";
+                                }
+
+                                if ($forth_answer != "0"){
+
+                                    echo " الاجابة الرابعة : " . $forth_answer. "<br>";
+
+                                }
+
+                            }
+
+
+
+                            ?>
+                        </div>
+                    </div>
+                    <div class="row">
+
+                        <p>السبب: <?php echo $reason ?></p>
                     </div>
 
                 </div>
 
             </div>
 
+        </div>
+
         <div class="line"></div>
+        <?php
+        $x++;
+    }  else{
+        ?>
+        <div class="tabs side-tabs tabs-bordered clearfix" id="tab-5">
+
+            <ul class="tab-nav clearfix">
+                <li><a href="#">  <?php echo "السؤال رقم: " .$number ;?> <i class="fa fa-times" aria-hidden="true"></i></a> </li>
+            </ul>
+
+            <div class="tab-container">
+
+                <div class="tab-content clearfix" style="direction: rtl" id="tabs-17">
+
+                    <div class="fancy-title title-bottom-border">
+                        <h3>السؤال:  <span><?php    echo $question ; ?> </span> </h3>
+                    </div>
+
+                    <div class="row" >
+                        <div class="col-sm-7">
+                            <img src="<?php echo $picture ?>" class="img-fluid img-thumbnail" >
+
+                        </div>
+
+                        <div class="col-sm-5">
+                            <h4> الاجابات : </h4>
+                            <?php
+                            if ($type == "response"){
+                                ?>
+
+
+                                <p <?php if ( ${'selector_' . $x} == "فرامل"){ echo "style= 'color: red'";} elseif ($right_answer == 'فرامل'){ echo "style='color: green;'";} ?> >فرامل</p>
+                                <p <?php if ( ${'selector_' . $x} == "رفع قدم عن الوقود"){ echo "style= 'color: red'";} elseif ($right_answer == 'رفع قدم عن الوقود'){ echo "style='color: green;'";} ?>>رفع قدم عن الوقود</p>
+                                <p <?php if ( ${'selector_' . $x} == "لا شئ"){ echo "style= 'color: red'";} elseif ($right_answer == 'لا شئ'){ echo "style='color: green;'";} ?>>لا شيء</p>
+
+
+                                <?php
+                            } elseif ($type == "yesNo"){
+
+                                echo " الاجابة الأولى : نعم " . "<br>";
+                                echo " الاجابة االثانية : لا " . "<br>";
+
+                            } elseif ($type == "numInp"){
+                                echo " الاجابة الصحيحة : " . $right_answer. "<br>";
+
+                            }elseif ($type == "multiChoice"){
+
+
+                                echo " الاجابة الأولى : " . $right_answer. "<br>";
+                                echo " الاجابة االثانية : " . $second_answer. "<br>";
+
+                                if ($third_answer != "0") {
+
+                                    echo " الاجابة الثالثة : " . $third_answer . "<br>";
+                                }
+
+                                if ($forth_answer != "0"){
+
+                                    echo " الاجابة الرابعة : " . $forth_answer. "<br>";
+
+                                }
+
+                            }
+
+
+
+                            ?>
+                        </div>
+                    </div>
+                    <div class="row">
+
+                        <p>السبب: <?php echo $reason ?></p>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="line"></div>
+        <?php
+        $x++;
+    }
+    ?>
+
+
+
 
 
         <?php
@@ -237,6 +363,7 @@ include 'scripts/db_connection.php';
 
             }
 
+    }
 
             }
             ?>
@@ -249,71 +376,71 @@ include 'scripts/db_connection.php';
         </div>
     </div>
 </section>
-
-
-
-<footer id="footer" style="background-color: #F5F5F5;border-top: 2px solid rgba(0,0,0,0.06);">
-
-    <div class="container" style="border-bottom: 1px solid rgba(0,0,0,0.06);">
-
-        <!-- Footer Widgets
-        ============================================= -->
-        <div class="footer-widgets-wrap clearfix">
-
-            <div class="col_full ">
-
-                <div class="widget clear-bottommargin-sm clearfix">
-
-                    <div class="row">
-
-                        <div class="col-md-6 bottommargin-sm text-center">
-                            <div class="footer-big-contacts">
-                                <span>Call Us:</span>
-                                +(31) 6 12345678
-                            </div>
-                        </div>
-
-                        <div class="col-md-6 bottommargin-sm text-center">
-                            <div class="footer-big-contacts">
-                                <span>Send an Email:</span>
-                                info@alrawitheorie.nl
-                            </div>
-                        </div>
-
-                    </div>
-
-                </div>
-
-            </div>
-
-        </div><!-- .footer-widgets-wrap end -->
-
-    </div>
-
-    <!-- Copyrights
-    ============================================= -->
-    <div id="copyrights" class="nobg">
-
-        <div class="container clearfix">
-
-            <div class="text-center">
-                Copyrights &copy; 2018 All Rights Reserved by Al Rawi Theorie.<br>
-                <div class="copyright-links"><a href="#">شروط الإستخدام</a> / <a href="#">سياسات الخصوصية</a></div>
-            </div>
-
-            <div class="text-center topmargin-sm">
-                Developed & Designed by <a href="www.el-semicolon.nl"> El-SemiColon; </a>
-            </div>
-
-        </div>
-
-    </div><!-- #copyrights end -->
-
-
-
-</footer><!-- #footer end -->
-
-
+<!---->
+<!---->
+<!---->
+<!--<footer id="footer" style="background-color: #F5F5F5;border-top: 2px solid rgba(0,0,0,0.06);">-->
+<!---->
+<!--    <div class="container" style="border-bottom: 1px solid rgba(0,0,0,0.06);">-->
+<!---->
+<!--        <!-- Footer Widgets-->
+<!--        ============================================= -->-->
+<!--        <div class="footer-widgets-wrap clearfix">-->
+<!---->
+<!--            <div class="col_full ">-->
+<!---->
+<!--                <div class="widget clear-bottommargin-sm clearfix">-->
+<!---->
+<!--                    <div class="row">-->
+<!---->
+<!--                        <div class="col-md-6 bottommargin-sm text-center">-->
+<!--                            <div class="footer-big-contacts">-->
+<!--                                <span>Call Us:</span>-->
+<!--                                +(31) 6 12345678-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!---->
+<!--                        <div class="col-md-6 bottommargin-sm text-center">-->
+<!--                            <div class="footer-big-contacts">-->
+<!--                                <span>Send an Email:</span>-->
+<!--                                info@alrawitheorie.nl-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!---->
+<!--                    </div>-->
+<!---->
+<!--                </div>-->
+<!---->
+<!--            </div>-->
+<!---->
+<!--        </div><!-- .footer-widgets-wrap end -->-->
+<!---->
+<!--    </div>-->
+<!---->
+<!--    <!-- Copyrights-->
+<!--    ============================================= -->-->
+<!--    <div id="copyrights" class="nobg">-->
+<!---->
+<!--        <div class="container clearfix">-->
+<!---->
+<!--            <div class="text-center">-->
+<!--                Copyrights &copy; 2018 All Rights Reserved by Al Rawi Theorie.<br>-->
+<!--                <div class="copyright-links"><a href="#">شروط الإستخدام</a> / <a href="#">سياسات الخصوصية</a></div>-->
+<!--            </div>-->
+<!---->
+<!--            <div class="text-center topmargin-sm">-->
+<!--                Developed & Designed by <a href="www.el-semicolon.nl"> El-SemiColon; </a>-->
+<!--            </div>-->
+<!---->
+<!--        </div>-->
+<!---->
+<!--    </div><!-- #copyrights end -->-->
+<!---->
+<!---->
+<!---->
+<!--</footer><!-- #footer end -->-->
+<!---->
+<!---->
 
 <!-- Go To Top
 ============================================= -->
